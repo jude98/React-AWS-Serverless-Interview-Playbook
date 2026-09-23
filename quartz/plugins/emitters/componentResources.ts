f51501071a69a7cf1115884a71e71eb278148d99
Jude <judithgeorgejoseph123@gmail.com>
2026-09-23 13:55:31 +0530

    Implement curriculum learning order and Next/Back document pagination

--- quartz/plugins/emitters/componentResources.ts
+++ quartz/plugins/emitters/componentResources.ts
@@ -6,6 +6,8 @@ import { QuartzEmitterPlugin } from "../types"
 import spaRouterScript from "../../components/scripts/spa.inline"
 // @ts-ignore
 import popoverScript from "../../components/scripts/popover.inline"
+// @ts-ignore
+import curriculumNavScript from "../../components/scripts/curriculumNav.inline"
 import baseStyles from "../../styles/base.scss"
 import customStyles from "../../styles/custom.scss"
 import popoverStyle from "../../components/styles/popover.scss"
@@ -257,6 +259,8 @@ function addGlobalPageResources(ctx: BuildCtx, componentResources: ComponentReso
       document.head.appendChild(rybbitScript);
     `)
   }
+
+  componentResources.afterDOMLoaded.push(curriculumNavScript)
 
   if (cfg.enableSPA) {
     componentResources.afterDOMLoaded.push(spaRouterScript)
